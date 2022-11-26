be140f5a44a241a586b3a725cc466b77df8237d4
William Houston <wmbhouston@gmail.com>
2022-11-26 09:14:59 -0500

    [WIP] Add account edit, update, destroy, and misc changes
    - Update database username after username renamed on pi
    - Add rails ujs (why is that not default?)

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class AccountsController < ApplicationController
+  before_action :load_account, only: [:edit, :update, :destroy]
+
   def index
     @accounts = Account.all.reorder([:account_type, :number])
   end
@@ -20,9 +22,31 @@ class AccountsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @account.update(allowed_params)
+      redirect_to accounts_path, notice: 'Successfully updated account'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @account.destroy
+      redirect_to accounts_path, notice: 'Successfully removed account'
+    else
+      redirect_to accounts_path, notice: 'Failed to remove account'
+    end
+  end
+
   private
 
   def allowed_params
     params.require(:account).permit(:name, :number, :account_type)
+  end
+
+  def load_account
+    @account = Account.find(params.fetch(:id))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :accounts, only: [:index, :new, :create]
+  resources :accounts, only: [:index, :new, :create, :edit, :update, :destroy]
 
   root 'accounts#index'
 end
